21891e936c2cd1c9a42250dd4214366306f79195
Justin Chenault <jchenault@kccmfg.com>
2026-03-09 14:58:12 -0400

    Initial Commit

---
+++ Automate.ps1
@@ -0,0 +1,7 @@
+$url="https://github.com/JChenaultKCCMFG/Automate/"
+$zip="C:\_KCC\Automate.zip"
+New-Item "C:\_KCC -Type Directory
+Invoke-WebRequest $url -OutFile $zip
+Expand-Archive $zip -DestinationPath "C:\_KCC\KCC_Automate" -Force
+Set-Location C:\_KCC\Automate
+msiexec /i "C:\_KCC\Agent_install.msi" TRANSFORMS="C:\_KCC\Agent_install.mst" /qn /L*v C:\_KCC\LGAuto-Install.log
